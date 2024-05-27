eba74ccf574ae261b47ee2935630ca775f74f1a1
Kristiyan Tishev <kristiyan.tishev@mentormate.com>
2024-05-27 22:58:36 +0300

    Implement reports

--- db/migrate/20240525214122_create_client_products.rb
+++ db/migrate/20240525214122_create_client_products.rb
@@ -1,12 +1,12 @@
 class CreateClientProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :client_products do |t|
-      t.integer :user_id
-      t.integer :product_id
+      t.references :user, index: true
+      t.references :product, index: true
 
       t.timestamps
     end
 
-    add_index :client_products, [:user_id, :product_id]
+    # add_index :client_products, [:user_id, :product_id]
   end
 end
